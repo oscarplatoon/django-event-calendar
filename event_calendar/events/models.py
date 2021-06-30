def6f8dc5a15a6c742adebe080bed12b95c33a85
Won Yang <won@gatech.edu>
2021-06-30 13:33:28 -0500

    CRD works. U is in progress

--- event_calendar/events/models.py
+++ event_calendar/events/models.py
@@ -1,9 +1,23 @@
 from django.db import models
-from django.utils import timezone 
+from django.utils import timezone
+
+# add validator that checks if starts_at < ends_at
+
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
-    starts_at = models.DateTimeField(default=timezone.now)
-    ends_at = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True, null=True)
+    starts_at = models.DateTimeField(
+        default=timezone.now, blank=False, null=False)
+    ends_at = models.DateTimeField(
+        default=timezone.now, blank=False, null=False)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"""
+            Name: {self.name}
+            Description: {self.description}
+            Starts at: {self.starts_at}
+            Ends at: {self.ends_at}
+            Created at: {self.created_at}        
+        """
--- event_calendar/events/views.py
+++ event_calendar/events/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Event
+from .forms import EventForm
 
 # Create your views here.
+
+
+def list_events(request):
+    events = Event.objects.all()
+    context = {'events': events}
+    return render(request, 'events/events_list.html', context)
+
+
+def show_event(request, event_id):
+    event = Event.objects.get(id=event_id)
+    context = {'event': event}
+    return render(request, 'events/event_detail.html', context)
+
+
+# A subclass of ModelForm can accept an existing model instance as the keyword argument instance; if this is supplied, YourForm.save() will update that instance. If it’s not supplied, YourForm.save() will create a new instance of the specified model.
+
+
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            new_event = form.save()
+            return redirect('events:show_event', event_id=new_event.id)
+    else:  # if request is GET, we create a blank form and display it
+        event_form = EventForm()
+        context = {'event_form': event_form}
+        return render(request, 'events/create_event.html', context)
+
+
+def edit_event(request, event_id):
+    event = Event.objects.get(id=event_id)
+    if request.method == 'POST':
+        # Passing in instance of unedited_event b/c we want to edit that instance with the POST data.
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            event = form.save()
+            event.save()
+            return redirect('events:show_event', event_id=event.id)
+    else:  # if request is GET
+        event_form = EventForm(instance=event)
+        context = {'event_form': event_form}
+        return render(request, 'events/create_event.html', context)
+
+
+def delete_event(request, event_id):
+    event = Event.objects.get(id=event_id)
+    event.delete()
+    return redirect('events:list_events')
